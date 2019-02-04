0f6b39900700932ea5314959779d6a84fe0a2318
Shohan Rahman <shohan2@gmail.com>
2019-02-04 23:35:10 +0600

    Design tableview cell for showing file and folder list.

---
+++ File Manager/File Manager/Home/View/FileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FileTableViewCell.swift
+//  File Manager
+//
+//  Created by Shohan Rahman on 2/4/19.
+//  Copyright © 2019 Shohan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class FileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
