b3b1e123cfd04e1bb4d6bf6986acdfe6fad81db9
Shohan Rahman <shohan2@gmail.com>
2019-02-04 23:48:10 +0600

    HomeViewModel file created.

---
+++ File Manager/File Manager/Home/View Model/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  File Manager
+//
+//  Created by Shohan Rahman on 2/4/19.
+//  Copyright © 2019 Shohan Rahman. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    
+    
+}
